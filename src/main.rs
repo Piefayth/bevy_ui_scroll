ec5172162aa3a2ea31394ddd4c6ea995f0c943bf
Sean Ryan <piefayth@gmail.com>
2023-03-05 14:58:39 -0600

    quick cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use bevy::{prelude::*};
 
-const FIXED_TIMESTEP: f32 = 0.1;
-
 use bevy_ui_scroll::background_scene::*;
 use bevy_ui_scroll::scroll::components::*;
 use bevy_ui_scroll::scroll::UiScrollPlugin;
@@ -13,8 +11,6 @@ fn main() {
         .add_plugin(UiScrollPlugin)
         .add_startup_system(setup_scene)
         .add_startup_system(spawn_scroll_container)
-        //.add_system(fixed_update.in_schedule(CoreSchedule::FixedUpdate))
-        .insert_resource(FixedTime::new_from_secs(FIXED_TIMESTEP))
         .run();
 }
 
